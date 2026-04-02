e6015094f7f616dae156801c15d0e840cd65a281
Enrique L <enrique.loyola@gmail.com>
2026-04-02 23:53:41 +0200

    trackbqll

---
+++ config/boards/shields/charybdis/charybdis_layers.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024 The ZMK Contributors
+ * SPDX-License-Identifier: MIT
+ * 
+ * Common layer definitions for Charybdis keyboard
+ * Shared across all shield variants
+ */
+
+#pragma once
+
+// Layer definitions matching the keymap
+#define BASE 0
+#define LOWER 1
+#define RAISE 2
+#define MOUSE 3
+#define CONFIG 4
+#define SCROLL 5
+#define SNIPING 6
